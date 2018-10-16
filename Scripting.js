2663409b9efa5f8cabd2cbced59ffb71159df8e3
Diego <diegov0824@hotmail.com>
2018-10-15 17:15:46 -0700

    Added layout for location variables in JS

--- Scripting.js
+++ Scripting.js
@@ -4,6 +4,15 @@
 var kickOffLocation;
 var activityOneLocation;
 var activityTwoLocation;
+var puzzleOneLocation;
+var puzzleTwoLocation;
+var puzzleThreeLocation;
+var puzzleFourLocation; 
+var puzzleFiveLocation;
+var puzzleSixLocation;
+var activityThreeLocation;
+var puzzleSevenLocation;
+var puzzleEightLocation;
 
 // START SCRIPTING FOR KICKOFF
 
@@ -32,57 +41,83 @@ function ActivityTwoLocationChanged(){
 
 // END SCRIPTING FOR ACTIVITY TWO PAGE
 
+// START SCRIPTING FOR ACTIVITY THREE PAGE
+
+function ActivityThreeLocationChanged(){
+    activityThree = document.getElementById("ActivityThreeLocation").value;
+}
+
+
+// END SCRIPTING FOR ACTIVITY THREE PAGE
+
 // START SCRIPTING FOR PUZZLE ONE
 
-
+function PuzzleOneLocationChanged(){
+    puzzleOneLocation = document.getElementById("PuzzleOneLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE ONE
 
 // START SCRIPTING FOR PUZZLE TWO
 
-
+function PuzzleTwoLocationChanged(){
+    puzzleTwoLocation = document.getElementById("PuzzleTwoLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE TWO
 
 // START SCRIPTING FOR PUZZLE THREE
 
-
+function PuzzleThreeLocationChanged(){
+    puzzleThreeLocation = document.getElementById("PuzzleThreeLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE THREE
 
 // START SCRIPTING FOR PUZZLE FOUR
 
-
+function PuzzleFourLocationChanged(){
+    puzzleFourLocation = document.getElementById("PuzzleFourLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE FOUR
 
 // START SCRIPTING FOR PUZZLE FIVE
 
-
+function PuzzleFiveLocationChanged(){
+    puzzleFiveLocation = document.getElementById("PuzzleFiveLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE FIVE
 
 // START SCRIPTING FOR PUZZLE SIX
 
+function PuzzleOneLocationChanged(){
+    puzzleSixLocation = document.getElementById("PuzzleSixLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE SIX
 
 // START SCRIPTING FOR PUZZLE SEVEN
 
-
+function PuzzleSevenLocationChanged(){
+    puzzleSevenLocation = document.getElementById("PuzzleSevenLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE SEVEN
 
 // START SCRIPTING FOR PUZZLE EIGHT
 
-
+function PuzzleEightLocationChanged(){
+    puzzleEightLocation = document.getElementById("PuzzleEightLocation").value;
+}
 
 
 // END SCRIPTING FOR PUZZLE EIGHT
